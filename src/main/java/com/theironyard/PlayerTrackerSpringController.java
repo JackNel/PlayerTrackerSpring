7d0ddafe4719e855f88b66884a3bfbc315d263e9
JackNel <jcneligan@gmail.com>
2015-11-12 17:18:22 -0500

    Add login/logout with secure password

--- src/main/java/com/theironyard/PlayerTrackerSpringController.java
+++ src/main/java/com/theironyard/PlayerTrackerSpringController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 
+
 /**
  * Created by Jack on 11/12/15.
  */
@@ -14,13 +15,40 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class PlayerTrackerSpringController {
     @Autowired
-    UserRepository users;
-    @Autowired
     PlayerRepository players;
+    @Autowired
+    UserRepository users;
 
     @RequestMapping("/")
     public String home(Model model, HttpSession session) {
-
+        String username = (String) session.getAttribute("username");
+        model.addAttribute("username", username);
+        model.addAttribute("players", players.findAll());
         return "home";
+    }
+
+    @RequestMapping("/login")
+    public String login(HttpSession session, String username, String password) throws Exception {
+        session.setAttribute("username", username);
+        User user = users.findOneByUsername(username);
+        if (user == null) {
+            user = new User();
+            user.username = username;
+            user.password = PasswordHash.createHash(password);
+            users.save(user);
+        }
+        else if (!PasswordHash.validatePassword(password, user.password)) {
+            throw new Exception("Wrong password, try again!");
+        }
+        else if (username == null || password == null) {
+            throw new Exception("Please enter both a username and password!");
+        }
+        return "redirect:/";
+    }
+
+    @RequestMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/";
     }
 }
