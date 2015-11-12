76e273d5ac47c0a27a7418ff7f3de863f708c2d8
JackNel <jcneligan@gmail.com>
2015-11-12 17:18:37 -0500

    Add login/logout with secure password

--- src/main/java/com/theironyard/UserRepository.java
+++ src/main/java/com/theironyard/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by Jack on 11/12/15.
  */
 public interface UserRepository extends CrudRepository<User, Integer> {
+    User findOneByUsername(String username);
 }
