e7e96b41f4e7d20c83ddfead98c433d0617539d0
JackNel <jcneligan@gmail.com>
2015-11-16 23:07:38 -0500

    Add tests, edit and delete tests not passing

--- src/main/java/com/theironyard/services/PlayerRepository.java
+++ src/main/java/com/theironyard/services/PlayerRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
  * Created by Jack on 11/12/15.
  */
 public interface PlayerRepository extends PagingAndSortingRepository<Player, Integer> {
+    Player findOneByName(String name);
 }
--- src/test/java/com/theironyard/PlayerTrackerSpringApplicationTests.java
+++ src/test/java/com/theironyard/PlayerTrackerSpringApplicationTests.java
@@ -1,18 +1,89 @@
 package com.theironyard;
 
+import com.theironyard.services.PlayerRepository;
+import com.theironyard.services.UserRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = PlayerTrackerSpringApplication.class)
 @WebAppConfiguration
 public class PlayerTrackerSpringApplicationTests {
+	@Autowired
+	PlayerRepository players;
+	@Autowired
+	UserRepository users;
+	@Autowired
+	WebApplicationContext wap;
+
+	MockMvc mockMvc;
+
+	@Before
+	public void before() {
+		players.deleteAll();
+		users.deleteAll();
+		mockMvc = MockMvcBuilders.webAppContextSetup(wap).build();
+	}
 
 	@Test
-	public void contextLoads() {
+	public void testLogin() throws Exception {
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/login")
+					.param("username", "testUser")
+					.param("password", "testPass")
+		);
+		assertTrue(users.count() == 1);
 	}
+
+	@Test
+	public void testCreate() throws Exception {
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/create")
+					.param("name", "testName")
+					.param("number", "1")
+					.param("team", "testTeam")
+					.param("position", "testPosition")
+					.param("age", "1")
+					.sessionAttr("username", "testUser")
+		);
+		assertTrue(players.count() == 1);
+	}
+
+	@Test
+	public void testEditPlayer() throws Exception {
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/edit-player")
+					.sessionAttr("username", "testUser")
+					.param("id", "1")
+					.param("name", "testName")
+					.param("number", "2")
+					.param("team", "testTeam")
+					.param("position", "testPosition")
+					.param("age", "2")
+		);
+		assertTrue(players.findOneByName("testName").age == 2);
+	}
+
+	@Test
+	public void testDelete() throws Exception {
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/delete")
+					.sessionAttr("username", "testUser")
+					.param("id", "1")
+		);
+		assertTrue(players.count() == 0);
+	}
+
 
 }
